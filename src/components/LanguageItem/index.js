cb8e26a8d264820206801e4198382662a47e9f64
saikumar <saikumargoudkotagiri2@gmail.com>
2023-02-07 13:35:49 +0000

    multilingualGreetings

---
+++ src/components/LanguageItem/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+
+const LanguageItem = props => {
+  const {isActive, onClickChangeLanguage, LanguageItemDetails} = props
+  const {buttonText, id} = LanguageItemDetails
+
+  const onClickButton = () => {
+    onClickChangeLanguage(id)
+  }
+  const activeTabBtnClassName = isActive ? 'highlighted' : 'not-highlighted'
+
+  return (
+    <li className="button-container">
+      <button
+        type="button"
+        // eslint-disable-next-line react/no-unknown-property
+        active={isActive}
+        className={`buttons ${activeTabBtnClassName}`}
+        onClick={onClickButton}
+      >
+        {buttonText}
+      </button>
+    </li>
+  )
+}
+export default LanguageItem
